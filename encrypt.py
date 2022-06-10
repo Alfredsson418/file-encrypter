99fcffabe81b41a45aea4d92bac97f4ea8231b53
TehTrooper <oskar.game@outlook.com>
2022-06-11 00:21:59 +0200

    Added Files

---
+++ encrypt.py
@@ -0,0 +1,28 @@
+# https://cryptography.io/en/latest/
+# https://realpython.com/python-command-line-arguments/#the-command-line-interface
+from re import T
+from cryptography.fernet import Fernet
+import sys
+args = sys.argv
+if len(args) < 3:
+    sys.exit("Missing Arguments!")
+
+encryptDir = args[1]
+keyDir = args[2]
+
+
+key = Fernet.generate_key()
+
+# Sparar Nyckeln
+with open(keyDir + "key.txt", "wb") as keyFile:
+    keyFile.write(key)
+
+f = Fernet(key)
+
+# Läser Filen och sparar innehållet som binärt 
+with open(encryptDir, "rb") as file:
+    token = f.encrypt(file.read())
+
+# Skriver över innehållet som binärt
+with open(encryptDir, "wb") as file:
+    file.write(token)
